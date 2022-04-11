0a581d71e87408303fe1b84a0eca00dd3d976c95
Pixel <53318360+Pixelking6409@users.noreply.github.com>
2022-04-12 06:52:36 +1200

    help.js almost

--- commands/help.js
+++ commands/help.js
@@ -8,27 +8,35 @@ module.exports = {
 
     execute(message, args, client) {
         let catagory = args[0].toLowerCase()
-        let showncatagory = catagory.charAt(0).toUpperCase() + catagory.slice(1)
         let prefix = client.prefix
 
         if (!catagory) {
+            let helpembed = new MessageEmbed()
+                .setTitle(`${client.user.username} commands`)
+                .setColor("RED")
+                .setFooter(`Requested by ${message.author.username}`, message.author.displayAvatarURL())
 
+            
+            
         } else {
+            let showncatagory = catagory.charAt(0).toUpperCase() + catagory.slice(1)
+
             let helpembed = new MessageEmbed()
                 .setTitle(`${showncatagory} commands`)
                 .setColor("RED")
+                .setFooter(`Requested by ${message.author.username}`, message.author.displayAvatarURL())
 
 
             let string = '';
             client.commands.forEach(command => {
                 console.log(command)
-                if (command.usage === catagory) {
+                if (command.type === catagory) {
                     string += `**${prefix}${command.name}**  ${command.usage}\n${command.description}\n`
                 }
             })
 
             helpembed.setDescription(string)
-            message.channel.send({ embeds: [helpembed] })
         } 
+        message.channel.send({ embeds: [helpembed] })
     }
 }
