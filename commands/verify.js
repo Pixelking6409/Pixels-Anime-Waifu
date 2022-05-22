5ad05fbee8d7e6e6477ef6f7250625b309cb8d2a
Pixel <53318360+Pixelking6409@users.noreply.github.com>
2022-05-23 00:53:53 +1200

    fuck me

--- commands/verify.js
+++ commands/verify.js
@@ -30,7 +30,7 @@ module.exports = {
             if (!collected.first().content.toUpperCase() === captcha.value) message.author.send("Verify Failed!");
             message.author.send("Verified Successfully!");
 
-            let r = await message.guild.roles.cache.get("977877260814147621");
+            let r = message.guild.roles.cache.get("977877260814147621");
             message.member.roles.add(r)
         })
     }
