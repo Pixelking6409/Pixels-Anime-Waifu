ca34ce7fe7fc42efd012c1b083182f0cd78642df
Pixel <53318360+Pixelking6409@users.noreply.github.com>
2022-05-23 00:51:20 +1200

    fix

--- commands/verify.js
+++ commands/verify.js
@@ -21,15 +21,17 @@ module.exports = {
 
         let msg = await message.author.send({ embeds: [verifyembed], files: [captcha.JPEGStream] })
         let filter = () => true;
-        let answer = await msg.channel.awaitMessages(filter, {
+        msg.channel.awaitMessages(filter, {
             maxMatches: 1,
             time: 60000
-        }).catch(console.log);
+        })
+        .catch(console.log)
+        .then(collected => {
+            if (!collected.first().content.toUpperCase() === captcha.value) message.author.send("Verify Failed!");
+            message.author.send("Verified Successfully!");
 
-        if (!answer.toUpperCase() === captcha.value) message.author.send("Verify Failed!");
-        message.author.send("Verified Successfully!");
-
-        let r = await message.guild.roles.cache.get("977877260814147621");
-        message.member.roles.add(r)
+            let r = await message.guild.roles.cache.get("977877260814147621");
+            message.member.roles.add(r)
+        })
     }
 }
