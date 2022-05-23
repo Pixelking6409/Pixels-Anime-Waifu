8ed303ae8021790b6d239d8c27e92b7cdba03dc5
Pixel <53318360+Pixelking6409@users.noreply.github.com>
2022-05-24 09:18:47 +1200

    d

--- commands/moderation/roleall.js
+++ commands/moderation/roleall.js
@@ -8,7 +8,7 @@ module.exports = {
     description: "Give everyone in the server a role",
     cooldown: "10",
 
-    async execute(message, args, client) {
+    execute(message, args, client) {
         if (!args[0]) return message.reply("Please mention a role!")
         if (!message.member.permissions.has('MANAGE_SERVER')) return message.reply("You dont have enough permissions for this command!")
 
@@ -21,5 +21,7 @@ module.exports = {
                 setTimeout(() => msg.delete(), 1000)
             })
         });
+
+        message.channel.send("Finished giving out roles!")
     }
 }
